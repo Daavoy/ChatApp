48d9882c0cdce8aff926b49a075afe6e8adb24ec
Daavoy <jan-petter.daavoy@outlook.com>
2024-06-18 20:12:21 +0200

    feat: will display basic message from server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
 
 import './App.css'
-
+import { useState, useEffect } from 'react'
+import { io } from 'socket.io-client'
 function App() {
+  const [time, setTime] = useState('fetching')
+  useEffect(() => {
+    const socket = io('http://localhost:5000')
+    socket.on('connect', () => console.log(socket.id))
+    socket.on('connect_error', () => { setTimeout(() => socket.connect(), 3000) })
 
+    socket.on('time', (data) => setTime(data))
+    socket.on('disconnect', () => setTime('server disconnected'))
+  }, [])
   return (
     <>
-      <p>hello</p>
+      {time}
     </>
   )
 }
