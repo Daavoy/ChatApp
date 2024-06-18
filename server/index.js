8a17855fddf45f16f181bf9657cd59cf90e13374
Daavoy <jan-petter.daavoy@outlook.com>
2024-06-18 20:11:28 +0200

    feat: basic server finished

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const socketIo = require('socket.io')
+const http = require('http')
+const PORT = process.env.PORT || 5000
+
+const app = express()
+const server = http.createServer(app)
+
+const io = socketIo(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        optionsSuccessStatus: 200
+    }
+})
+
+io.on('connection', (socket) => {
+    console.log('client connected:', socket.id)
+    socket.join('clock-room')
+    socket.on('disconnect', (reason) => {
+        console.log(reason)
+    })
+})
+
+setInterval(() => {
+    io.to('clock-room').emit('time', new Date())
+}, 1000)
+
+server.listen(PORT, err => {
+    if (err) {
+        console.error(err)
+    }
+    console.log('Server running on port:', PORT)
+})
