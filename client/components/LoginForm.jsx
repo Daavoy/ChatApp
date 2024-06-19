5c53449e9e0f5ce05834c6bdee9d04c8129d1fb1
Daavoy <jan-petter.daavoy@outlook.com>
2024-06-19 21:00:47 +0200

    feat: add login form component

---
+++ client/components/LoginForm.jsx
@@ -0,0 +1,27 @@
+
+import { useState, useRef } from "react"
+export const LoginForm = ({ onFormSubmit }) => {
+    const [userName, setUserName] = useState('');
+    const formRef = useRef(null);
+
+    const onChange = ((e) => {
+        setUserName(e.target.value);
+    })
+
+    const onSubmit = ((e => {
+        e.preventDefault();
+        console.log("form submitted with username:", userName);
+        onFormSubmit(userName);
+        setUserName('');
+        formRef.current.reset();
+    }))
+
+    return (
+        <div>
+            <form onSubmit={onSubmit} ref={formRef}>
+                <label htmlFor="userName">Enter a username: </label><input type="text" id="userName" placeholder="Enter a username" onChange={onChange} />
+                <button type="submit">Submit</button>
+            </form>
+        </div>)
+
+}
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,44 @@
 
 import './App.css'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { io } from 'socket.io-client'
+import { LoginForm } from '../components/LoginForm'
+import Chat from '../components/Chat'
 function App() {
   const [time, setTime] = useState('fetching')
+  const [submittedUserName, setSubmittedUserName] = useState("");
+  const socketRef = useRef(null);
+
+  const handleUserNameSubmit = (userName) => {
+    setSubmittedUserName(userName)
+    joinRoom(userName)
+  }
+
   useEffect(() => {
-    const socket = io('http://localhost:5000')
+    socketRef.current = io('http://localhost:5000');
+    const socket = socketRef.current;
     socket.on('connect', () => console.log(socket.id))
     socket.on('connect_error', () => { setTimeout(() => socket.connect(), 3000) })
 
     socket.on('time', (data) => setTime(data))
     socket.on('disconnect', () => setTime('server disconnected'))
+    return () => {
+      socket.disconnect();
+    };
   }, [])
+
+  const joinRoom = (userName) => {
+    console.log(`Submitted username: ${userName}`)
+    if (userName === "") {
+      return;
+    }
+    socketRef.current.emit("join_room", userName)
+  }
+
   return (
     <>
+      <LoginForm onFormSubmit={handleUserNameSubmit} />
+      <Chat socket={socketRef.current} userName={submittedUserName} />
       {time}
     </>
   )
