71e2be3418260879815961b248084a6d7ecd23f7
Daavoy <jan-petter.daavoy@outlook.com>
2024-06-20 19:56:48 +0200

    refactor: refactor chat and app to handle routing

--- client/components/Chat.jsx
+++ client/components/Chat.jsx
@@ -1,9 +1,16 @@
 /* eslint-disable react/prop-types */
 import React from "react"
+import "../styles/Chat.css"
 import { useState, useEffect } from "react";
-function Chat({ socket, userName, room }) {
+import { useLocation, useNavigate } from "react-router-dom";
+import { useSocket } from "../context/SocketContext";
+const Chat = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+    const socket = useSocket();
     const [currentMessage, setCurrentMessage] = useState("");
     const [messageList, setMessageList] = useState([]);
+    const { userName } = location.state || {}; // Access the state passed from Home
 
 
     const sendMessage = async () => {
@@ -11,19 +18,22 @@ function Chat({ socket, userName, room }) {
             return;
         }
 
-        console.log(userName)
         const messageData = {
             sender: userName,
             message: currentMessage,
             time: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes(),
         }
         setMessageList((prevList) => [...prevList, messageData]);
+        setCurrentMessage("");
         await socket.emit("send_message", messageData);
     }
     useEffect(() => {
         if (socket) {
             socket.on("receive_message", (data) => {
                 setMessageList((prevList) => [...prevList, data]);
+            })
+            socket.on('disconnect', () => {
+                navigate("/", { state: { errorMessage: "User disconnected from server" } });
             })
         }
     }, [socket]);
@@ -36,17 +46,17 @@ function Chat({ socket, userName, room }) {
             <div className="chat-header">
 
             </div>
-            <div className="chat-body">
+            <div className="chat-body hide-scrollbar">
                 {messageList.map((message, idx) => {
                     return (
                         <div key={idx} className="message">
-                            <p><strong>{message.sender}:</strong> {message.message} <span>{message.time}</span></p>
+                            <p><strong>{message.sender}:</strong> {message.message} <span>{message.time}</span> </p>
                         </div>
                     );
                 })}
             </div>
             <div className="chat-footer">
-                <input type="text" onChange={handleChange} onKeyDown={(event) => event.key === "Enter" && sendMessage()} />
+                <input type="text" onChange={handleChange} onKeyDown={(event) => event.key === "Enter" && sendMessage()} value={currentMessage} />
                 <button onClick={sendMessage}>&#9658;</button>
             </div>
         </div>
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,46 +1,26 @@
 
 import './App.css'
-import { useState, useEffect, useRef } from 'react'
-import { io } from 'socket.io-client'
-import { LoginForm } from '../components/LoginForm'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Chat from '../components/Chat'
+import Home from '../components/Home';
+import { SocketProvider } from '../context/SocketContext';
 function App() {
-  const [time, setTime] = useState('fetching')
-  const [submittedUserName, setSubmittedUserName] = useState("");
-  const socketRef = useRef(null);
 
-  const handleUserNameSubmit = (userName) => {
-    setSubmittedUserName(userName)
-    joinRoom(userName)
-  }
 
-  useEffect(() => {
-    socketRef.current = io('http://localhost:5000');
-    const socket = socketRef.current;
-    socket.on('connect', () => console.log(socket.id))
-    socket.on('connect_error', () => { setTimeout(() => socket.connect(), 3000) })
-
-    socket.on('time', (data) => setTime(data))
-    socket.on('disconnect', () => setTime('server disconnected'))
-    return () => {
-      socket.disconnect();
-    };
-  }, [])
-
-  const joinRoom = (userName) => {
-    console.log(`Submitted username: ${userName}`)
-    if (userName === "") {
-      return;
-    }
-    socketRef.current.emit("join_room", userName)
-  }
+  // <Chat socket={socketRef.current} userName={submittedUserName} />
+  // {time}
 
   return (
-    <>
-      <LoginForm onFormSubmit={handleUserNameSubmit} />
-      <Chat socket={socketRef.current} userName={submittedUserName} />
-      {time}
-    </>
+    <SocketProvider>
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+
+      </Router>
+    </SocketProvider>
   )
 }
 
